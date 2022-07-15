f784e77374aab51974c7aa4da62d2aeb9b92060b
Alan Wolfe <alan.wolfe@gmail.com>
2022-07-14 20:34:02 -0700

    some progress

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#define _CRT_SECURE_NO_WARNINGS
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 #include <random>
 #include <vector>
 #include <array>
+#include <direct.h>
 
 static const int c_numPoints = 100;
-#define DETERMINISTIC() false
+#define DETERMINISTIC() true
 
 
 typedef std::array<float, 2> Vec2;
@@ -34,11 +37,29 @@ Vec2 Calculate3rdEquilateralTrianglePoint(const Vec2& A, const Vec2& B)
 }
 
 template <typename T, size_t N>
+std::array<T, N> operator+(const std::array<T, N>& A, const std::array<T, N>& B)
+{
+    std::array<T, N> ret;
+    for (size_t i = 0; i < N; ++i)
+        ret[i] = A[i] + B[i];
+    return ret;
+}
+
+template <typename T, size_t N>
 std::array<T, N> operator-(const std::array<T, N>& A, const std::array<T, N>& B)
 {
     std::array<T, N> ret;
     for (size_t i = 0; i < N; ++i)
         ret[i] = A[i] - B[i];
+    return ret;
+}
+
+template <typename T, size_t N>
+std::array<T, N> operator*(const std::array<T, N>& A, const T& B)
+{
+    std::array<T, N> ret;
+    for (size_t i = 0; i < N; ++i)
+        ret[i] = A[i] * B;
     return ret;
 }
 
@@ -49,6 +70,34 @@ T Dot(const std::array<T, N>& A, const std::array<T, N>& B)
     for (size_t i = 0; i < N; ++i)
         ret += A[i] * B[i];
     return ret;
+}
+
+float SmoothStep(float edge0, float edge1, float x)
+{
+    if (x < edge0)
+        return 0;
+
+    if (x >= edge1)
+        return 1;
+
+    // Scale/bias into [0..1] range
+    x = (x - edge0) / (edge1 - edge0);
+
+    return x * x * (3 - 2 * x);
+}
+
+float Lerp(float A, float B, float t)
+{
+    return A * (1.0f - t) + B * t;
+}
+
+float Clamp(float value, float themin, float themax)
+{
+    if (value <= themin)
+        return themin;
+    if (value >= themax)
+        return themax;
+    return value;
 }
 
 // TODO: may not need this, dot, or the - operator
@@ -89,9 +138,99 @@ bool PointInTriangle(const Vec2& P, const Vec2& A, const Vec2& B, const Vec2& C)
         bary[2] >= 0.0f && bary[2] <= 1.0f;
 }
 
+bool BaryPointInTriangle(const Vec3& bary)
+{
+    return
+        bary[0] >= 0.0f && bary[0] <= 1.0f &&
+        bary[1] >= 0.0f && bary[1] <= 1.0f &&
+        bary[2] >= 0.0f && bary[2] <= 1.0f;
+}
+
+Vec2 BarycentricToCartesian(const Vec3& bary, const Vec2& A, const Vec2& B, const Vec2& C)
+{
+    return A * bary[0] + B * bary[1] + C * bary[2];
+}
+
+float MinDistanceBetweenBaryPoints(const Vec3& candidatePos, const Vec3& point)
+{
+    Vec2 A = BarycentricToCartesian(candidatePos, PointA, PointB, PointC);
+    Vec2 B = BarycentricToCartesian(point, PointA, PointB, PointC);
+
+    // direct distance
+    float distance = sqrtf(Dot(B - A, B - A));
+
+    // TODO: deal with edges!
+    return distance;
+}
+
+void DrawPoints(const std::vector<Vec3>& points)
+{
+    float minx = std::min(PointA[0], std::min(PointB[0], PointC[0]));
+    float maxx = std::max(PointA[0], std::max(PointB[0], PointC[0]));
+    float miny = std::min(PointA[1], std::min(PointB[1], PointC[1]));
+    float maxy = std::max(PointA[1], std::max(PointB[1], PointC[1]));
+
+    float offsety = 0.5f - (maxy - miny) / 2.0f;
+
+    static const int c_imgSize = 256;
+
+    std::vector<unsigned char> pixels(c_imgSize * c_imgSize * 3, 255);
+
+    unsigned char* pixel = pixels.data();
+    for (int iy = 0; iy < c_imgSize; ++iy)
+    {
+        // TODO: offset uv.y by offsety.
+        Vec2 uv = { 0.0f, 1.0f - (offsety + float(iy) / float(c_imgSize)) };
+        for (int ix = 0; ix < c_imgSize; ++ix)
+        {
+            uv[0] = float(ix) / float(c_imgSize);
+
+            Vec3 bary = Barycentric(uv, PointA, PointB, PointC);
+
+            Vec3 color = Vec3{ 1.0f, 1.0f, 1.0f };
+
+            // handle edges
+            if (BaryPointInTriangle(bary))
+            {
+                float offset = -0.01f;
+
+                float dist = std::min(std::min(std::abs(bary[0]+ offset), std::abs(bary[1]+ offset)), std::abs(bary[2]+ offset));
+                float shade = SmoothStep(0.0025f, 0.01f, dist);
+
+                color[0] = Lerp(0.1f, 1.0f, shade);
+                color[1] = Lerp(0.1f, 1.0f, shade);
+                color[2] = Lerp(0.1f, 1.0f, shade);
+            }
+
+            // handle points
+            for (const Vec3& point : points)
+            {
+                Vec2 pointCar = BarycentricToCartesian(point, PointA, PointB, PointC);
+
+                float dist = sqrt(Dot(pointCar - uv, pointCar - uv));
+                float shade = 1.0f - SmoothStep(0.0025f, 0.01f, dist);
+
+                color[0] = Lerp(color[0], 0.0f, shade);
+                color[1] = Lerp(color[1], 0.0f, shade);
+                color[2] = Lerp(color[2], 1.0f, shade);
+            }
+
+            pixel[0] = (unsigned char)Clamp(color[0] * 255.0f, 0.0f, 255.0f);
+            pixel[1] = (unsigned char)Clamp(color[1] * 255.0f, 0.0f, 255.0f);
+            pixel[2] = (unsigned char)Clamp(color[2] * 255.0f, 0.0f, 255.0f);
+
+            pixel += 3;
+        }
+    }
+
+    stbi_write_png("out/points.png", c_imgSize, c_imgSize, 3, pixels.data(), 0);
+}
+
 int main(int argc, char** argv)
 {
-    std::vector<Vec2> points;
+    _mkdir("out");
+
+    std::vector<Vec3> points;
 
 #if DETERMINISTIC()
     std::mt19937 rng;
@@ -103,26 +242,27 @@ int main(int argc, char** argv)
 
     for (int pointIndex = 0; pointIndex < c_numPoints; ++pointIndex)
     {
+        // Generate multiple candidates for the next point and keep the best one
         int numCandidates = (int)points.size() + 1;
-
-        Vec2 bestCandidatePos;
+        Vec3 bestCandidatePos;
         float bestCandidateScore = -FLT_MAX;
-
         for (int candidateIndex = 0; candidateIndex < numCandidates; ++candidateIndex)
         {
             // Find a point in the triangle through rejection sampling
-            Vec2 candidatePos = Vec2{ dist(rng), dist(rng) };
-            while (!PointInTriangle(candidatePos, PointA, PointB, PointC))
-                candidatePos = Vec2{ dist(rng), dist(rng) };
-
-            float candidateScore = FLT_MAX;
-            for (const Vec2& point : points)
+            Vec3 candidatePos = Vec3{ dist(rng), dist(rng), 0.0f };
+            candidatePos[2] = 1.0f - candidatePos[0] - candidatePos[1];
+            while (!BaryPointInTriangle(candidatePos))
             {
-                float distance = DistanceBetweenPoints();
+                candidatePos = Vec3{ dist(rng), dist(rng), 0.0f };
+                candidatePos[2] = 1.0f - candidatePos[0] - candidatePos[1];
             }
 
-            // TODO: calculate score
+            // calculate the distance to the nearest point
+            float candidateScore = FLT_MAX;
+            for (const Vec3& point : points)
+                candidateScore = std::min(candidateScore, MinDistanceBetweenBaryPoints(candidatePos, point));
 
+            // keep the candidate that has the largest distance to the nearest point
             if (candidateScore > bestCandidateScore)
             {
                 bestCandidateScore = candidateScore;
@@ -134,17 +274,15 @@ int main(int argc, char** argv)
         points.push_back(bestCandidatePos);
     }
 
+    DrawPoints(points);
+
     return 0;
 }
 
-// TODO: switch to working in barycentric coordinates, not actual coordinates. generate u, v, calculate w.
-
 /*
 TODO:
-- draw out the points to an image. different numbers of points
+- draw out the points to an image. different numbers of points. Could draw a non equilateral triangle too.
 - draw out the points tiled randomly
 - generate candidates using distances at each of the 3 edges, 3 orientations, and flipped/not
-
-- TODO: convert points to barycentric coordinates
 
 */
